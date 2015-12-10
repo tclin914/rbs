7c1899d7536f29fba9cc4a2f1a194cf558019063
tclin <tclin914@gmail.com>
2015-12-10 16:04:23 +0800

    print login slogan successfully

--- hw3.c
+++ hw3.c
@@ -12,30 +12,60 @@
 #include <fcntl.h>
 #include <errno.h>
 
+#define F_CONNECTING 0
+#define F_READING    1
+#define F_WRITING    2
+#define F_DONE       3
+
 void parseString(char* string, int* nbHost);
 int readline(int fd,char *ptr,int maxlen);
+void printMsg(char* string);
 char* hosts[5];
 char* ports[5];
 char* files[5];
 
+const char* head = "<html>"
+                   "<head>"
+                   "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=big5\" />"
+                   "<title>Network Programming Homework 3</title>"
+                   "</head>";
+const char* body = "<body bgcolor=#336699>";
+const char* font = "<font face=\"Courier New\" size=2 color=#FFFF99>";
+const char* table1 = "<table width=\"800\" border=\"1\">"
+                     "<tr>";
+const char* table_element = "<td>%s</td>";
+const char* table2 = "</tr>"
+                     "<tr>"
+                     "<td valign=\"top\" id=\"m0\"></td><td valign=\"top\" id=\"m1\"></td><td valign=\"top\" id=\"m2\"></td>"
+                     "<td valign=\"top\" id=\"m3\"></td><td valign=\"top\" id=\"m4\"></td>"
+                     "</tr>"
+                     "</table>";
+const char* script = "<script>document.all[\'m0\'].innerHTML += \"%s<br>\";</script>";
+const char* tail = "</font>"
+                   "</body>"
+                   "</html>";
+const char* br = "<br>";
+
 int main(int argc,char *argv[])
 {
-    printf("Content-type:text/html\n\n");
-    printf("<head>");
-    printf("<title>Hello world it is cgi test</title>\n");
-    printf("</head>\n");
-    printf("<body>\n");
-    printf("<h1>Hello world </h1>\n");
-    printf("</body>\n");
+    printf(head);
+    printf(body);
+    printf(font);
+    printf(table1);
 
-    char* queryString = getenv("QUERY_STRING");
-    printf("queryString = %s\n", queryString);
-    printf("<br>\n");
+	char* queryString = getenv("QUERY_STRING");
+    /* printf("queryString = %s<br>\n", queryString); */
 
     int nbHost;
     parseString(queryString, &nbHost);
-    printf("nbHost = %d\n", nbHost);
+    /* printf("nbHost = %d<br>\n", nbHost); */
 
+    for (int i = 0; i < nbHost; i++) {
+        printf(table_element, hosts[i]);
+    }
+
+    printf(table2);
+    
     int n;
     int host_fd[5] = {0};
     struct sockaddr_in host_sin[5];
@@ -49,22 +79,59 @@ int main(int argc,char *argv[])
         /* set nonblock */
         int flags = fcntl(host_fd[i], F_GETFL, 0);
         fcntl(host_fd[i], F_SETFL, flags | O_NONBLOCK);
-        if ((n = connect(host_fd[i], (struct sockaddr*)&host_sin[i], sizeof(host_sin))) < 0) {
+        if ((n = connect(host_fd[i], (struct sockaddr*)&host_sin[i], sizeof(host_sin[i]))) < 0) {
             if (errno != EINPROGRESS) ;
         }
     }
     
-    printf("debug\n");
-    printf("<br>\n");
     printf("fd = %d\n", host_fd[0]);
 
-    char buff[2048];
-    while (1) {
-        n = readline(host_fd[0], buff, strlen(buff) + 1);
-        printf("n = %d\n", n);
-        n = write(host_fd[0], "ls", 3);
-        printf(buff);
+    fd_set rfds;
+    fd_set wfds;
+    fd_set rs;
+    fd_set ws;
+
+    int conn = 1;
+    int nfds = FD_SETSIZE;
+    FD_ZERO(&rfds);
+    FD_ZERO(&wfds);
+    FD_ZERO(&rs);
+    FD_ZERO(&ws);
+    
+    FD_SET(host_fd[0], &rs);
+    FD_SET(host_fd[0], &ws);
+
+    int status = F_CONNECTING;
+    int error;
+    char buf[15000];
+    while (conn > 0) {
+        memcpy(&rfds, &rs, sizeof(rfds));
+        memcpy(&wfds, &ws, sizeof(wfds));
+
+        if (select(nfds, &rfds, &wfds, (fd_set*)0, (struct timeval*)0) < 0) printf("select error\n");
+
+        if (status == F_CONNECTING && (FD_ISSET(host_fd[0], &rfds) || FD_ISSET(host_fd[0], &wfds))) {
+            if (getsockopt(host_fd[0], SOL_SOCKET, SO_ERROR, &error, &n) < 0 || error != 0) {
+                printf("<br>non-blocking fail\n");
+            }
+            status = F_READING;
+            printf("<br>F_CONNECTING\n");
+        } else if (status == F_WRITING && FD_ISSET(host_fd[0], &wfds)) {
+            n = write(host_fd[0], "ls", 3);
+            status = F_READING;
+        } else if (status == F_READING && FD_ISSET(host_fd[0], &rfds)) {
+            printf("<br>");
+            n = read(host_fd[0], buf, sizeof(buf) - 1);
+            printMsg(buf);
+            n = write(host_fd[0], "ls", 3);
+            /* status = F_WRITING; */
+            /* FD_CLR(host_fd[0], &rs); */
+            /* conn--; */
+        }
     }
+    close(host_fd[0]);
+
+    /* printf(tail); */
     return 0;
 }
 
@@ -77,6 +144,7 @@ void parseString(char* string, int* nbHost) {
         switch (count % 3) {
             case 0:
                 hosts[count / 3] = (pair + 3);
+                if (strlen(pair + 3) > 0) (*nbHost)++;
                 printf("%s\n", hosts[count / 3]);
                 break;
             case 1:
@@ -91,7 +159,6 @@ void parseString(char* string, int* nbHost) {
         ++count;
         pair = strtok(NULL, "&");
     }
-    *nbHost = (count / 3);
 }
 
 int readline(int fd,char *ptr,int maxlen) {
@@ -116,3 +183,18 @@ int readline(int fd,char *ptr,int maxlen) {
 	return(n);
 }
 
+void printMsg(char* string) {
+    char* tmp = string;
+    int c = 0;
+    while (*tmp != '\0') {
+        if (*tmp == '\n') {
+            char* msg = strndup(string, c);
+            printf(script, msg);
+            string = ++tmp;
+            c = 0;
+            continue;
+        }
+        ++tmp;
+        ++c;
+    }
+}
